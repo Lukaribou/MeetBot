7821c1b9a54540010bdec3c8303dbd11ef85d9ec
Lukaribou <lucasperroud17@gmail.com>
2020-10-17 14:32:44 +0200

    Update run.py

--- meetbot/run.py
+++ meetbot/run.py
@@ -8,22 +8,23 @@ from meetbot.core.MeetBot import MeetBot
 from meetbot.config import TOKEN
 
 
-def load_commands() -> List[Command]:
+def load_commands(do_not_load: List[str] = []) -> List[Command]:
     cmds = []
     for file in os.listdir('./commands/'):
-        if not file == '__pycache__':
+        if file not in do_not_load:
             # https://stackoverflow.com/questions/67631/how-to-import-a-module-given-the-full-path
             spec = importlib.util.spec_from_file_location(f'meetbot.commands.{file}', f'./commands/{file}')
             mod = importlib.util.module_from_spec(spec)
             spec.loader.exec_module(mod)
             cmds.append(mod.FileCommand())
-
     return cmds
 
 
 if __name__ == '__main__':
     bot = MeetBot(debug_mode=True)
 
-    bot.cmds.add_commands(load_commands())
+    bot.cmds.add_commands(
+        load_commands(['__pycache__'])
+    )
 
     bot.run(TOKEN)
