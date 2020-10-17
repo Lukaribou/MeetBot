22d32269e0f8df7d8252b004d65ed9f37e0d13b4
Lukaribou <lucasperroud17@gmail.com>
2020-10-17 16:42:21 +0200

    Update ping.py

--- meetbot/commands/ping.py
+++ meetbot/commands/ping.py
@@ -1,3 +1,7 @@
+import time
+
+import discord.embeds
+
 from meetbot.core.Commands import Command, Context
 
 
@@ -6,4 +10,10 @@ class FileCommand(Command):
         super().__init__("ping")
 
     async def run(self, ctx: Context):
-        await ctx.channel.send("pong")
+        em = discord.embeds.Embed(color=0x00FF00)
+        t1 = time.time()
+        m = await ctx.channel.send(embed=discord.embeds.Embed(color=0xFF0000))
+        em.add_field(
+            name='Latence API:',
+            value=str((time.time() - t1) * 1000).split('.')[0] + 'ms')
+        await m.edit(embed=em)
