54beda70ea20b566ab53305c2417a12c62f27528
Lukaribou <lucasperroud17@gmail.com>
2020-10-20 22:55:39 +0200

    Update DataBase.py

--- meetbot/core/DataBase.py
+++ meetbot/core/DataBase.py
@@ -1,24 +1,35 @@
 import mariadb
 import sys
+from typing import Tuple, Union
+
+from meetbot.config import DB_USER, DB_PSWD
 
 
 # https://mariadb.com/docs/appdev/connector-python/
 class DataBase:
-    def __init__(self, user: str, password: str, host: str, port: int):
+    def __init__(self, host: str, port: int, database: str):
         self._conn = None
         try:
             self._conn = mariadb.connect(
-                user=user,
-                password=password,
+                user=DB_USER,
+                password=DB_PSWD,
                 host=host,
                 port=port,
+                database=database,
                 autocommit=True)
         except mariadb.Error as e:
             print(f'Error connecting to DataBase: {e}')
             sys.exit(1)
 
-        self.cursor = self._conn.cursor()
+        self.cursor = self._conn.cursor(named_tuple=True)
 
-    def disconnect(self):
+    def disconnect(self) -> None:
         print(f'Déconnection de la base de données...')
         self._conn.close()
+
+    def execute(self, cmd: str, *args):
+        self.cursor.execute(cmd, args)
+        return self.cursor
+
+    def profile_exist(self, author_id):
+        return len(self.execute("SELECT * FROM profiles WHERE user_id = ?", str(author_id)).fetchall()) != 0
