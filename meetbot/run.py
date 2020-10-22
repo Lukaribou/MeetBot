1a5b27fff5a09228a6bea759b079297254ecf236
Lukaribou <lucasperroud17@gmail.com>
2020-10-22 18:17:30 +0200

    argv parameters

--- meetbot/run.py
+++ meetbot/run.py
@@ -1,9 +1,13 @@
+import sys
+
 from meetbot.core.MeetBot import MeetBot
 from meetbot.config import TOKEN
 
 
 if __name__ == '__main__':
-    bot = MeetBot(debug_mode=True)
+    mods = sys.argv[1:]
+
+    bot = MeetBot(debug_mode='-d' in mods, maintenance_mode='-m' in mods)
 
     bot.cmds.load_commands_from_dir(do_not_load=['__pycache__'])
 
