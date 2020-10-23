bc57ae2fd5d9329ca45abf1058b493213760166e
Lukaribou <lucasperroud17@gmail.com>
2020-10-23 23:34:55 +0200

    Profile command

--- meetbot/commands/profile.py
+++ meetbot/commands/profile.py
@@ -29,6 +29,28 @@ class FileCommand(Command):
             target = Profile(ctx.db, target.id) if ctx.db.profile_exist(target.id) else None
 
         if target is None or not target:
-            await ctx.channel.send(EMOJIS['x'] + ' **The provided id/user does not match any profile.**')
+            return await ctx.channel.send(EMOJIS['x'] + ' **The provided id/user does not match any profile.**')
+        if not target.active:
+            return await ctx.channel.send(EMOJIS['x'] + ' **This profile is not active.**')
 
-        em = discord.embeds.Embed(color=target.color, title='In')
+        user: discord.User = ctx.bot.get_user(target.user_id)
+
+        def t(a):
+            if type(a) is str:
+                return a if a is not '' else 'Not provided'
+            else:
+                return a if a is not 0 else 'Not provided'
+
+        em = discord.embeds.Embed(color=target.color)
+        em.set_author(name='Informations about: ' + user.name, icon_url=user.avatar_url)
+        em.add_field(name='Name:', value=t(target.name))
+        em.add_field(name='Gender:', value=t(target.gender))
+        em.add_field(name='Age:', value=str(t(target.age)))
+        em.add_field(name='Description:', value=t(target.description))
+        em.add_field(name='Country:', value=t(target.country))
+        em.add_field(name='Profile creation date:', value=str(target.creation_date))
+        em.add_field(name='Last meet command:', value=str(target.creation_date))
+        em.add_field(name='Other:', value=t(target.other))
+        em.set_footer(text='User id: ' + str(target.user_id))
+
+        await ctx.channel.send(embed=em)
--- meetbot/utils/Profile.py
+++ meetbot/utils/Profile.py
@@ -1,13 +1,12 @@
 from datetime import datetime
 
-from typing import List
 from meetbot.core.DataBase import DataBase
 
 
 class Profile:
-    def __init__(self, db: DataBase, user_id: str):
+    def __init__(self, db: DataBase, user_id: int):
         self._db = db
-        d = self._db.execute("SELECT * FROM profiles WHERE user_id = ?", user_id).fetchall()[0]
+        d = self._db.execute("SELECT * FROM profiles WHERE user_id = ?", str(user_id)).fetchall()[0]
 
         self.user_id = user_id
         self.name: str = d["name"]
@@ -15,7 +14,7 @@ class Profile:
         self.description: str = d["description"]
         self.age: int = d["age"]
         self.country: str = d["country"]
-        self.other: List[str] = [] if d["other"] is '' else [x for x in d["other"].split(";;")]
+        self.other: str = d["other"]
         self.creation_date: datetime = d["creation_date"]
         self.active: bool = bool(d["active"])
         self.last_meet: datetime = d["last_meet"]
